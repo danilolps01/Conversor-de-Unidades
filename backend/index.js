df2c76008ca0cc1c1b7a4d45e1a05b4d1f6adc2b
diego5x <deigo555g@gmail.com>
2025-11-24 19:36:23 -0300

    feat: Pagina de documentação da api

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const path = require("path");
 const app = express();
 
 const conversaoRoutes = require('./routes/conversaoRoutes');
 
 app.use(express.json());
 app.use('/api', conversaoRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 
 if (process.env.NODE_ENV !== 'test') {
     app.listen(8080, () => {
