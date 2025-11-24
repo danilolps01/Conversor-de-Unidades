7f1a276fb5deda6a426cc2c0e32b12615e7ae1ea
Gladyson Rhuan <140732508+1Rhuan@users.noreply.github.com>
2025-11-24 23:21:09 +0000

    feat: adicionar suporte a CORS e configurar .gitignore

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require("path");
+const cors = require('cors')
 const app = express();
 
 const conversaoRoutes = require('./routes/conversaoRoutes');
@@ -7,6 +8,7 @@ const conversaoRoutes = require('./routes/conversaoRoutes');
 app.use(express.json());
 app.use('/api', conversaoRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
